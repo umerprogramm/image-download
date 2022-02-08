241e0814f0fa6ad1177fce544ed2efb8291f780a
Umer <umerprogrammer@gmail.com>
2022-02-08 10:03:58 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+const GetInput = ()=>{
+    let VideoId = document.getElementById('input').value
+    
+    download(VideoId)
+  document.getElementById('input').value = ""
+}
+const download = (id)=>{
+    let source = document.getElementById('video').src = `https://www.youtube.com/embed/${id}`
+
+    
+}
+
+
+
+
+
