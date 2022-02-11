ce063a153b08426adbdb3d5e5a6ffb51d3344d8e
Umer <umerprogrammer@gmail.com>
2022-02-11 09:31:35 -0800

    First commit

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-const GetInput = ()=>{
-    let VideoId = document.getElementById('input').value
-    
-    download(VideoId)
-  document.getElementById('input').value = ""
-}
-const download = (id)=>{
-    let source = document.getElementById('video').src = `https://www.youtube.com/embed/${id}`
-    
-}
+const express = require('express')
+const app = express()
+const os = require('os');
+const path = require('path')
+const port = 3000
+app.set("view engine", "ejs")
 
+app.get('/', (req, res) => {
+res.render('index')
+})
+app.get('/s', (req, res) => {
+  res.send(req.body.download)
+  })
 
-
+app.listen(port, () => {
+  
+  console.log(`App listening on port ${port}`)
+})
 
